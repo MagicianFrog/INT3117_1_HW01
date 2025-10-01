43d6ef09079d25c1cc1465d8059f069bf944672a
MagicianFrog <anhkhang091205@gmail.com>
2025-10-01 17:07:04 +0700

    Update version1

--- AlexConfidence/src/main/java/Solution.java
+++ AlexConfidence/src/main/java/Solution.java
@@ -6,6 +6,33 @@ public class Solution {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        System.out.println(Version1.version1(a, b, c));
+
+        if ((a < 0) || (a > 20) || (b < 0) || (b > 6) || (c < 0) || (c > 4)) {
+            System.out.println("INVALID");
+            return;
+        }
+
+        double confidence = 0;
+        for (int i = 1; i <= a; i++) {
+            if (i <= 6) {
+                confidence += 8;
+            } else if (i <= 13) {
+                confidence += (8 + 2);
+            } else {
+                confidence += (8 + 6);
+            }
+        }
+
+        confidence -= b * 20;
+
+        if (c >= 2) {
+            confidence += confidence * 0.15;
+        }
+
+        if (confidence >= 80) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
     }
 }
--- AlexConfidence/src/main/java/Version1.java
+++ AlexConfidence/src/main/java/Version1.java
@@ -1,9 +1,19 @@
 public class Version1 {
     public static String version1(int a, int b, int c) {
         double confidence = 0;
-        confidence += a * 10;
+        for (int i = 1; i <= a; i++) {
+            if (i <= 6) {
+                confidence += 8;
+            } else if (i <= 13) {
+                confidence += 8 + 2;
+            } else {
+                confidence += 8 + 6;
+            }
+        }
         confidence -= b * 20;
-        confidence += confidence * (c * 0.05);
+        if (c >= 2) {
+            confidence += confidence * 0.15;
+        }
         return confidence >= 80 ? "PASS" : "FAIL";
     }
 }
