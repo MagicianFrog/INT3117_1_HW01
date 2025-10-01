9b81b489d5725b528a615d3a8049868e35146f20
MagicianFrog <anhkhang091205@gmail.com>
2025-10-01 18:07:40 +0700

    Rename function & Add DecisionTableTest

--- AlexConfidence/src/test/java/BoundaryTest.java
+++ AlexConfidence/src/test/java/BoundaryTest.java
@@ -6,99 +6,99 @@ public class BoundaryTest {
     // Normal a
     @Test
     public void boundaryTest1() {
-        assertEquals("PASS", Version1.version1(10, 0, 0));
+        assertEquals("PASS", Version1.solution(10, 0, 0));
     }
 
     @Test
     public void boundaryTest2() {
-        assertEquals("FAIL", Version1.version1(10, 6, 4));
+        assertEquals("FAIL", Version1.solution(10, 6, 4));
     }
 
     @Test
     public void boundaryTest3() {
-        assertEquals("FAIL", Version1.version1(10, 1, 1));
+        assertEquals("FAIL", Version1.solution(10, 1, 1));
     }
 
     @Test
     public void boundaryTest4() {
-        assertEquals("INVALID", Version1.version1(10, -1, -1));
+        assertEquals("INVALID", Version1.solution(10, -1, -1));
     }
 
     @Test
     public void boundaryTest5() {
-        assertEquals("INVALID", Version1.version1(10, 7, 5));
+        assertEquals("INVALID", Version1.solution(10, 7, 5));
     }
 
     @Test
     public void boundaryTest6() {
-        assertEquals("FAIL", Version1.version1(10, 5, 3));
+        assertEquals("FAIL", Version1.solution(10, 5, 3));
     }
 
     // Normal b
     @Test
     public void boundaryTest7() {
-        assertEquals("FAIL", Version1.version1(0, 3, 0));
+        assertEquals("FAIL", Version1.solution(0, 3, 0));
     }
 
     @Test
     public void boundaryTest8() {
-        assertEquals("PASS", Version1.version1(20, 3, 4));
+        assertEquals("PASS", Version1.solution(20, 3, 4));
     }
 
     @Test
     public void boundaryTest9() {
-        assertEquals("FAIL", Version1.version1(1, 3, 1));
+        assertEquals("FAIL", Version1.solution(1, 3, 1));
     }
 
     @Test
     public void boundaryTest10() {
-        assertEquals("INVALID", Version1.version1(-1, 3, -1));
+        assertEquals("INVALID", Version1.solution(-1, 3, -1));
     }
 
     @Test
     public void boundaryTest11() {
-        assertEquals("INVALID", Version1.version1(21, 3, 5));
+        assertEquals("INVALID", Version1.solution(21, 3, 5));
     }
 
     @Test
     public void boundaryTest12() {
-        assertEquals("PASS", Version1.version1(19, 3, 3));
+        assertEquals("PASS", Version1.solution(19, 3, 3));
     }
 
     // Normal c
     @Test
     public void boundaryTest13() {
-        assertEquals("FAIL", Version1.version1(0, 0, 2));
+        assertEquals("FAIL", Version1.solution(0, 0, 2));
     }
 
     @Test
     public void boundaryTest14() {
-        assertEquals("PASS", Version1.version1(20, 6, 2));
+        assertEquals("PASS", Version1.solution(20, 6, 2));
     }
 
     @Test
     public void boundaryTest15() {
-        assertEquals("FAIL", Version1.version1(1, 1, 2));
+        assertEquals("FAIL", Version1.solution(1, 1, 2));
     }
 
     @Test
     public void boundaryTest16() {
-        assertEquals("INVALID", Version1.version1(-1, -1, 2));
+        assertEquals("INVALID", Version1.solution(-1, -1, 2));
     }
 
     @Test
     public void boundaryTest17() {
-        assertEquals("INVALID", Version1.version1(21, 7, 2));
+        assertEquals("INVALID", Version1.solution(21, 7, 2));
     }
 
     @Test
     public void boundaryTest18() {
-        assertEquals("PASS", Version1.version1(19, 5, 2));
+        assertEquals("PASS", Version1.solution(19, 5, 2));
     }
 
     // All normal
     @Test
     public void boundaryTest19() {
-        assertEquals("FAIL", Version1.version1(10, 3, 2));
+        assertEquals("FAIL", Version1.solution(10, 3, 2));
     }
 }
---
+++ AlexConfidence/src/test/java/DecisionTableTest.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DecisionTableTest {
+
+    private String version(int a, int b, int c) {
+        return Version1.solution(a, b, c);
+    }
+
+    @Test
+    public void DecisionTableTest1() {
+        assertEquals("INVALID", version(-3, 5, 0));
+    }
+
+    @Test
+    public void DecisionTableTest2() {
+        assertEquals("INVALID", version(5, -1, 2));
+    }
+
+    @Test
+    public void DecisionTableTest3() {
+        assertEquals("INVALID", version(5, 3, 5));
+    }
+
+    @Test
+    public void DecisionTableTest4() {
+        assertEquals("FAIL", version(4, 4, 1));
+    }
+
+    @Test
+    public void DecisionTableTest5() {
+        assertEquals("FAIL", version(6, 3, 2));
+    }
+
+    @Test
+    public void DecisionTableTest6() {
+        assertEquals("FAIL", version(13, 2, 0));
+    }
+
+    @Test
+    public void DecisionTableTest7() {
+        assertEquals("PASS", version(11, 1, 3));
+    }
+
+    @Test
+    public void DecisionTableTest8() {
+        assertEquals("PASS", version(19, 2, 1));
+    }
+
+    @Test
+    public void DecisionTableTest9() {
+        assertEquals("PASS", version(16, 1, 3));
+    }
+
+
+}
